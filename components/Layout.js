2c0af814019e5b7f7a8e947f960eafc7c1ddae4d
nwshane <django09@gmail.com>
2017-05-13 10:16:52 +0400

    Styled the Layout with a max-width.

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,12 @@ import Header from './Header'
 
 export default (props) => (
   <div>
+    <style jsx>{`
+      div {
+        max-width: 900px;
+        margin: 0 auto;
+      }
+    `}</style>
     <Header />
     {props.children}
   </div>
