1d266d55c46135256d171b76b18aa72ff2231fd9
nwshane <django09@gmail.com>
2017-05-13 10:25:26 +0400

    Set global font-size.

---
+++ components/GlobalStyle.js
@@ -0,0 +1,9 @@
+export default () => (
+  <style jsx global>
+    {`
+      html {
+        font-size: 10px;
+      }
+    `}
+  </style>
+)
--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,19 @@
 import Header from './Header'
+import GlobalStyle from './GlobalStyle'
 
 export default (props) => (
   <div>
-    <style jsx>{`
-      div {
-        max-width: 900px;
-        margin: 0 auto;
-      }
-    `}</style>
+    <GlobalStyle />
     <Header />
     {props.children}
+    <style jsx>
+      {`
+        div {
+          max-width: 900px;
+          margin: 0 auto;
+          font-size: 1.6rem;
+        }
+      `}
+    </style>
   </div>
 )
