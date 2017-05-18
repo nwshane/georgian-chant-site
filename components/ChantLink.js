7d35c9bebb66377d1c7d2786fd53161f374bca3e
nwshane <django09@gmail.com>
2017-05-18 11:00:56 +0400

    Fixed ChantLink error resulting from next/link component having more than one child.

--- components/ChantLink.js
+++ components/ChantLink.js
@@ -6,16 +6,18 @@ const ChantLink = props => {
   const slug = props.data.get('slug')
 
   return (
-    <Link as={`/${locale}/chants/${slug}`} href={`/chants/show?slug=${slug}`}>
-      <a>
-        {props.data.get('name').get('ka')}
-      </a>
+    <div>
+      <Link as={`/${locale}/chants/${slug}`} href={`/chants/show?slug=${slug}&locale=${locale}`}>
+        <a>
+          {props.data.get('name').get('ka')}
+        </a>
+      </Link>
       <style jsx>{`
         a {
           text-decoration: none;
         }
       `}</style>
-    </Link>
+    </div>
   )
 }
 
