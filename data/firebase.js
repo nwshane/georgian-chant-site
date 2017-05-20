51201dc9a8ea3c106f48698bf6e9b5275ec854f7
nwshane <django09@gmail.com>
2017-05-20 07:46:52 +0400

    Simplified firebase implementation code.

--- data/firebase.js
+++ data/firebase.js
@@ -1,18 +1,17 @@
 import firebase from 'firebase'
 
-let app = firebase
-
-if (typeof window === 'object') {
-  app = firebase.initializeApp({
+const getFirebaseApp = () => (
+  firebase.apps.length === 0 ? firebase.initializeApp({
     apiKey: 'AIzaSyANmrGU6f8MiUGC_gxs4BeB8kRZQuMWuNc',
     authDomain: 'georgian-chant-site.firebaseapp.com',
     databaseURL: 'https://georgian-chant-site.firebaseio.com',
     projectId: 'georgian-chant-site',
     storageBucket: 'georgian-chant-site.appspot.com',
     messagingSenderId: '730123756383'
-  })
-}
+  }) : firebase.apps[0]
+)
+
+let app = getFirebaseApp()
 
 export default app
-
 export const database = app.database()
