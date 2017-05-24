1614132047121468c6b9cdf8bac0c310ea47e39e
nwshane <django09@gmail.com>
2017-05-24 14:59:59 +0400

    Moved MuiThemeProvider to Layout from LoginForm.

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,25 +3,29 @@ import type { Children } from 'react'
 import Header from './Header'
 import GlobalStyle from './GlobalStyle'
 import GlobalHead from './GlobalHead'
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import '~/components/tapEvents'
 
 const Layout = (props : { children?: Children }) => (
-  <div>
-    <GlobalStyle />
-    <GlobalHead />
-    <Header />
-    {props.children}
-    <style jsx>
-      {`
-        div {
-          max-width: 900px;
-          margin: 0 auto;
-          font-size: 1.6rem;
-          font-family: 'Open Sans', sans-serif;
-          padding: 0 10px;
-        }
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <div>
+      <GlobalStyle />
+      <GlobalHead />
+      <Header />
+      {props.children}
+      <style jsx>{`
+      div {
+        max-width: 900px;
+        margin: 0 auto;
+        font-size: 1.6rem;
+        font-family: 'Open Sans', sans-serif;
+        padding: 0 10px;
+      }
       `}
-    </style>
-  </div>
+      </style>
+    </div>
+  </MuiThemeProvider>
 )
 
 export default Layout
--- components/LoginForm.js
+++ components/LoginForm.js
@@ -3,9 +3,6 @@ import { auth } from '~/data/firebase'
 import { Form } from 'formsy-react'
 import { FormsyText } from 'formsy-material-ui/lib/'
 import RaisedButton from 'material-ui/RaisedButton'
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import '~/components/tapEvents'
 
 type Event = {
   target: {
@@ -45,47 +42,46 @@ class LoginForm extends Component {
 
   render () {
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme()}>
+      <div>
+        <Form
+          onSubmit={this.handleSubmit}
+          onValid={this.enableButton}
+          onInvalid={this.disableButton}
+        >
+          <FormsyText
+            id='input-email'
+            value=''
+            name='email'
+            title='Email'
+            validations='isEmail'
+            validationError='This is not a valid email'
+            floatingLabelText='Email'
+            required
+          />
+          <FormsyText
+            id='input-password'
+            value=''
+            name='password'
+            title='Password'
+            type='password'
+            floatingLabelText='Password'
+            required
+          />
+          <RaisedButton type='submit' disabled={!this.state.canSubmit}>
+            Sign In
+          </RaisedButton>
+        </Form>
         <div>
-          <Form
-            onSubmit={this.handleSubmit}
-            onValid={this.enableButton}
-            onInvalid={this.disableButton}
-          >
-            <FormsyText
-              id='input-email'
-              value=''
-              name='email'
-              title='Email'
-              validations='isEmail'
-              validationError='This is not a valid email'
-              floatingLabelText='Email'
-              required
-            />
-            <FormsyText
-              id='input-password'
-              value=''
-              name='password'
-              title='Password'
-              type='password'
-              floatingLabelText='Password'
-              required
-            />
-            <RaisedButton type='submit' disabled={!this.state.canSubmit}>
-              Sign In
-            </RaisedButton>
-          </Form>
-          <div>
-            {this.state.error}
-          </div>
-          <style jsx>{`
-            div :global(form) {
-              display: flex;
-              flex-direction: column;
-            }
-          `}</style>
+          {this.state.error}
         </div>
-      </MuiThemeProvider>
+        <style jsx>{`
+          div :global(form) {
+            display: flex;
+            flex-direction: column;
+          }
+          `}
+        </style>
+      </div>
     )
   }
 }
