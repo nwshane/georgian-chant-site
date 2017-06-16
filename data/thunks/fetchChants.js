c80070839e4dccdc2b3cebb0f0d32bbe55a5d3e1
nwshane <django09@gmail.com>
2017-06-16 07:56:03 +0400

    Changed fetchChants thunk to return promise so that the page waits for chants to load before rendering server-side.

--- data/thunks/fetchChants.js
+++ data/thunks/fetchChants.js
@@ -2,8 +2,8 @@
 import { database } from '~/data/firebase'
 import { setChants } from '~/data/ducks/chants'
 
-export default () => (dispatch: Function) => {
+export default () => (dispatch: Function) => (
   database.ref().child('chants').once('value', (snapshot) => {
     dispatch(setChants(snapshot.val()))
   })
-}
+)
