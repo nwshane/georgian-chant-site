1856e3c6fb7f5e5a10f3c525dde830184dc573da
nwshane <django09@gmail.com>
2017-06-21 05:32:02 +0400

    Made name colmn of admin chant table into link to chant show page.

--- components/Admin/ChantsTable.js
+++ components/Admin/ChantsTable.js
@@ -1,11 +1,13 @@
 // @flow
 import { Table, TableHeader, TableBody, TableRow, TableRowColumn } from 'material-ui/Table'
 import LocalizedLink from '~/components/LocalizedLink'
+import ChantLink from '~/components/ChantLink'
 import map from 'lodash.map'
 import { connect } from 'react-redux'
 import { getChants } from '~/data/ducks/chants'
 import type { Chants, State } from '~/data/types'
 
+// TODO: Localize
 const AdminChantsTable = ({chants}: {chants: Chants}) => (
   <Table>
     <TableHeader
@@ -25,7 +27,9 @@ const AdminChantsTable = ({chants}: {chants: Chants}) => (
     >
       {map(chants, (chant, key) => (
         <TableRow key={key}>
-          <TableRowColumn>{chant.name.ka}</TableRowColumn>
+          <TableRowColumn>
+            <ChantLink chant={chant} />
+          </TableRowColumn>
           <TableRowColumn>
             <LocalizedLink
               as={`/admin/chants/${chant.slug}/edit`}
