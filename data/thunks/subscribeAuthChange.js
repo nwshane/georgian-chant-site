61d4db4c209915452bb6a5b516102d0c3ebb6df1
nwshane <django09@gmail.com>
2017-06-22 13:03:02 +0400

    Changed subscribeAuthChange so that it refreshes the cookie when token is refreshed by firebase.

--- data/thunks/subscribeAuthChange.js
+++ data/thunks/subscribeAuthChange.js
@@ -2,9 +2,9 @@
 import { auth } from '~/data/firebase'
 import { setCurrentUser } from '~/data/ducks/currentUser'
 import type { Dispatch, User } from '~/data/types'
-import getTokenCookieFromServer from '~/helpers/getTokenCookieFromServer'
 import { eraseCookie, readCookie } from '~/helpers/cookieHelpers'
 import { FIREBASE_ID_TOKEN_COOKIE } from '~/universal/constants'
+import axios from 'axios'
 
 let subscribed = false
 
@@ -14,15 +14,25 @@ export default () => (dispatch: Dispatch, getState: Function) => {
   if (subscribed) return
   subscribed = true
 
-  return auth.onAuthStateChanged((currentUser: User) => {
+  return auth.onIdTokenChanged(async function (currentUser: User) {
     dispatch(setCurrentUser(currentUser))
 
-    if (currentUser && !readCookie(FIREBASE_ID_TOKEN_COOKIE)) {
-      getTokenCookieFromServer()
+    if (!currentUser) {
+      if (readCookie(FIREBASE_ID_TOKEN_COOKIE)) {
+        eraseCookie(FIREBASE_ID_TOKEN_COOKIE)
+      }
+
+      return
     }
 
-    if (!currentUser && readCookie(FIREBASE_ID_TOKEN_COOKIE)) {
-      eraseCookie(FIREBASE_ID_TOKEN_COOKIE)
+    const idToken = await auth.currentUser.getIdToken(true)
+
+    // if cookie does not exist, or the token was refreshed, get
+    // a new cookie with correct token
+    if (readCookie(FIREBASE_ID_TOKEN_COOKIE) !== idToken) {
+      axios.post('/authWithIdToken', {
+        idToken
+      })
     }
   })
 }
