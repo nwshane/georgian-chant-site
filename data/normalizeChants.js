ada7e96cb56f1e81f4f265c5bbaaf9bb5436033c
nwshane <django09@gmail.com>
2017-07-20 08:30:00 -0400

    Returning empty object from normalizeChants if chants is empty.

--- data/normalizeChants.js
+++ data/normalizeChants.js
@@ -3,20 +3,24 @@ import type { Chants, Chant } from '~/data/types'
 
 const flattenObjectIntoKeysArray = (recordings) => (
   recordings && !Array.isArray(recordings)
-  ? Object.keys(recordings)
-  : recordings
+    ? Object.keys(recordings)
+    : recordings
 )
 
 const normalizeChant = (chant: Chant): Chant => (
-  chant ? {
-    ...chant,
-    recordings: flattenObjectIntoKeysArray(chant.recordings)
-  } : chant
+  chant
+    ? {
+      ...chant,
+      recordings: flattenObjectIntoKeysArray(chant.recordings)
+    }
+    : chant
 )
 
 export default (chants: Chants): Chants => (
-  Object.keys(chants).reduce((normalizedChants, key) => {
-    normalizedChants[key] = normalizeChant(chants[key])
-    return normalizedChants
-  }, {})
+  chants
+    ? Object.keys(chants).reduce((normalizedChants, key) => {
+      normalizedChants[key] = normalizeChant(chants[key])
+      return normalizedChants
+    }, {})
+    : {}
 )
