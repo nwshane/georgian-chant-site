39c8497828ef71b2945dcce83df332333ddabc67
nwshane <django09@gmail.com>
2017-08-02 10:50:35 -0400

    Changed global request logger from console.log to winston.debug.

--- server/initializeGlobalRequestLogger.js
+++ server/initializeGlobalRequestLogger.js
@@ -1,14 +1,12 @@
 const globalLog = require('global-request-logger')
+const winston = require('winston')
+
 globalLog.initialize()
 
 globalLog.on('success', function (request, response) {
-  console.log('SUCCESS')
-  console.log('Request', request)
-  console.log('Response', response)
+  winston.debug('Global Request Logger: Successful outgoing request. Request:', request, 'Response: ', response)
 })
 
 globalLog.on('error', function (request, response) {
-  console.log('ERROR')
-  console.log('Request', request)
-  console.log('Response', response)
+  winston.debug('Global Request Logger: Errored outgoing request. Request:', request, 'Response: ', response)
 })
