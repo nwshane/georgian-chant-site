fd29b5bbe810b182d61fd8c201a8008a8f310c04
nwshane <django09@gmail.com>
2017-08-03 08:34:19 -0400

    Factored out getFirebaseAdminInitConfig function into separate file, and corrected databaseURL.

--- server/authenticationMiddleware.js
+++ server/authenticationMiddleware.js
@@ -1,39 +1,14 @@
 const { FIREBASE_ID_TOKEN_COOKIE } = require('../universal/constants')
 const firebaseAdmin = require('firebase-admin')
 const winston = require('winston')
+const getFirebaseAdminInitConfig = require('./getFirebaseAdminInitConfig')
 
-const getFirebaseCredentials = () => {
-  const {
-    FIREBASE_TYPE: type,
-    FIREBASE_PROJECT_ID: projectId,
-    FIREBASE_PRIVATE_KEY_ID: privateKeyId,
-    FIREBASE_PRIVATE_KEY: privateKey,
-    FIREBASE_CLIENT_EMAIL: clientEmail,
-    FIREBASE_CLIENT_ID: clientId,
-    FIREBASE_AUTH_URI: authUri,
-    FIREBASE_TOKEN_URI: tokenUri,
-    FIREBASE_AUTH_PROVIDER_CERT_URL: authProviderX509CertUrl,
-    FIREBASE_CLIENT_CERT_URL: clientX509CertUrl
-  } = process.env
+winston.info(
+  'Initializing Firebase Admin with Config:',
+  getFirebaseAdminInitConfig()
+)
 
-  return {
-    type,
-    projectId,
-    privateKeyId,
-    privateKey,
-    clientEmail,
-    clientId,
-    authUri,
-    tokenUri,
-    authProviderX509CertUrl,
-    clientX509CertUrl
-  }
-}
-
-firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(getFirebaseCredentials()),
-  databaseURL: 'https://georgian-chant-site.firebaseio.com'
-})
+firebaseAdmin.initializeApp(getFirebaseAdminInitConfig())
 
 async function decodeToken (firebaseIdToken) {
   try {
---
+++ server/getFirebaseAdminInitConfig.js
@@ -0,0 +1,39 @@
+const firebaseAdmin = require('firebase-admin')
+
+const getFirebaseCredentials = () => {
+  const {
+    FIREBASE_TYPE: type,
+    FIREBASE_PROJECT_ID: projectId,
+    FIREBASE_PRIVATE_KEY_ID: privateKeyId,
+    FIREBASE_PRIVATE_KEY: privateKey,
+    FIREBASE_CLIENT_EMAIL: clientEmail,
+    FIREBASE_CLIENT_ID: clientId,
+    FIREBASE_AUTH_URI: authUri,
+    FIREBASE_TOKEN_URI: tokenUri,
+    FIREBASE_AUTH_PROVIDER_CERT_URL: authProviderX509CertUrl,
+    FIREBASE_CLIENT_CERT_URL: clientX509CertUrl
+  } = process.env
+
+  return {
+    type,
+    projectId,
+    privateKeyId,
+    privateKey,
+    clientEmail,
+    clientId,
+    authUri,
+    tokenUri,
+    authProviderX509CertUrl,
+    clientX509CertUrl
+  }
+}
+
+const getDatabaseUrl = () => {
+  const { projectId } = getFirebaseCredentials()
+  return `https://${projectId}.firebaseio.com`
+}
+
+module.exports = () => ({
+  credential: firebaseAdmin.credential.cert(getFirebaseCredentials()),
+  databaseURL: getDatabaseUrl()
+})
