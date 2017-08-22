4652fef7b3e0899ca6c0db17bd9547f502b9715f
nwshane <django09@gmail.com>
2017-08-22 11:47:15 -0400

    Created separate log functions for logging all args and for logging just one arg and returning it.

--- helpers/log.js
+++ helpers/log.js
@@ -1,5 +1,10 @@
-export default function () {
+export function logArgs () {
   const args = Array.from(arguments)
   console.log(...args)
   return args[args.length - 1]
+}
+
+export const log = (obj) => {
+  console.log(obj)
+  return obj
 }
