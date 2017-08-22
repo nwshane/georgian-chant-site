201dfe6c48853e756810bf669ac225b9cecaa647
nwshane <django09@gmail.com>
2017-08-22 11:16:17 -0400

    Added ability to search by the latin transliterated text.

--- components/ChantList.js
+++ components/ChantList.js
@@ -5,12 +5,15 @@ import { compose, curry, filter } from 'ramda'
 import type { Chants, State } from '~/data/types'
 import { getChants } from '~/data/ducks/chants'
 import { getSearch } from '~/data/ducks/filters/search'
+import latinizeGeorgian from 'latinize-georgian'
 
 const filterBySearch = curry((search: string, chants: Chants): Chants => (
   filter((chant) => (
     chant.name.ka.includes(search) ||
+      latinizeGeorgian(chant.name.ka).includes(search) ||
       chant.name.en.includes(search) ||
       chant.text.ka.includes(search) ||
+      latinizeGeorgian(chant.text.ka).includes(search) ||
       chant.text.en.includes(search)
   ), chants)
 ))
