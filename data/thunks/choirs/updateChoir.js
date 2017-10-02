2976fff5f93623109e3771b44231ef91dcaeec10
nwshane <django09@gmail.com>
2017-10-02 15:13:24 -0400

    Implemented updateChoir thunk.

--- data/thunks/choirs/updateChoir.js
+++ data/thunks/choirs/updateChoir.js
@@ -1,12 +1,50 @@
 // @flow
-import type {Choir, Dispatch} from '~/data/types'
+import { database } from '~/data/firebase'
+import convertNameToSlug from '~/helpers/convertNameToSlug'
+import { setAppMessage } from '~/data/ducks/appMessage'
+import type { Choir, Dispatch } from '~/data/types'
+import redirectToLocalizedUrl from '~/helpers/redirectToLocalizedUrl'
 
-const updateChoir = (slug: string, choirValues: Choir) => async function (dispatch: Dispatch) {
-  console.log('Updating choir', {
-    slug,
-    choirValues
-  })
-  return null
+export default (oldSlug: string, choirValues: Choir) => async function (dispatch: Dispatch) {
+  const newChoirValues = {
+    ...choirValues,
+    slug: convertNameToSlug(choirValues.name.ka)
+  }
+
+  const { slug: newSlug } = newChoirValues
+
+  try {
+    await database
+    .ref()
+    .child('choirs')
+    .child(newSlug)
+    .update(newChoirValues)
+
+    if (oldSlug !== newSlug) {
+      const oldChoir = await database
+      .ref()
+      .child('choirs')
+      .child(oldSlug)
+
+      oldChoir.remove()
+    }
+
+    dispatch(setAppMessage({
+      text: 'Choir updated successfully',
+      category: 'success'
+    }))
+
+    redirectToLocalizedUrl(
+      {
+        pathname: '/choirs/show',
+        query: { slug: newChoirValues.slug }
+      },
+      { pathname: `/choirs/${newChoirValues.slug}` }
+    )
+  } catch (e) {
+    dispatch(setAppMessage({
+      text: 'Choir could not be updated',
+      category: 'error'
+    }))
+  }
 }
-
-export default updateChoir
