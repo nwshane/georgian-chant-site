2be65ebb1fb876925768181e2b143be4fae144d3
nwshane <django09@gmail.com>
2017-10-03 15:21:39 -0400

    Made SheetMusicTable component into an actual table.

---
+++ components/shared/SheetMusicTable/SheetMusicTableRow.js
@@ -0,0 +1,37 @@
+// @flow
+import React from 'react'
+import { TableRow, TableRowColumn } from 'material-ui/Table'
+import type {SheetMusicScore, School} from '~/data/types'
+import {getSchool} from '~/data/ducks/schools'
+import {connect} from 'react-redux'
+import {getTranslatedName} from '~/data/getters'
+import {injectIntl} from 'react-intl'
+import type {IntlShape} from 'react-intl'
+
+type Props = {
+  sheetMusicScore: SheetMusicScore,
+  school: School,
+  intl: IntlShape
+}
+
+const SheetMusicTableRow = (props: Props) => {
+  const {sheetMusicScore, school, intl: {locale}} = props
+  return (
+    <TableRow>
+      <TableRowColumn>
+        {getTranslatedName(locale, school)}
+      </TableRowColumn>
+      <TableRowColumn>
+        <a target='_blank' href={sheetMusicScore.url}>
+          View
+        </a>
+      </TableRowColumn>
+    </TableRow>
+  )
+}
+
+const mapStateToProps = (state, props) => ({
+  school: getSchool(props.sheetMusicScore.school, state)
+})
+
+export default injectIntl(connect(mapStateToProps)(SheetMusicTableRow))
--- components/shared/SheetMusicTable/index.js
+++ components/shared/SheetMusicTable/index.js
@@ -1,22 +1,39 @@
 // @flow
 import React from 'react'
+import { Table, TableHeader, TableBody, TableRow, TableRowColumn } from 'material-ui/Table'
 import type {SheetMusic} from '~/data/types'
 import {getSheetMusic} from '~/data/ducks/sheetMusic'
 import {mapObjIndexed, values} from 'ramda'
 import {connect} from 'react-redux'
+import SheetMusicTableRow from './SheetMusicTableRow'
 
 type Props = {
   sheetMusic: SheetMusic
 }
 
 const SheetMusicTable = ({sheetMusic}: Props) => (
-  <ul>
-    {values(mapObjIndexed((sheetMusicScore, key) => (
-      <li key={key}>
-        {sheetMusicScore.url}
-      </li>
-    ), sheetMusic))}
-  </ul>
+  <Table>
+    <TableHeader
+      displaySelectAll={false}
+      adjustForCheckbox={false}
+    >
+      <TableRow>
+        <TableRowColumn>
+          Chant School
+        </TableRowColumn>
+        <TableRowColumn>
+          Link
+        </TableRowColumn>
+      </TableRow>
+    </TableHeader>
+    <TableBody
+      displayRowCheckbox={false}
+    >
+      {values(mapObjIndexed((sheetMusicScore, key) => (
+        <SheetMusicTableRow {...{sheetMusicScore, key}} />
+      ), sheetMusic))}
+    </TableBody>
+  </Table>
 )
 
 const mapStateToProps = (state) => ({
